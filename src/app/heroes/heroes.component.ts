63204f03c64a64191f88615fbc24029576547380
zoraiard <zoraiarodrigues@gmail.com>
2019-05-22 21:54:22 -0300

    Revert "reate a HeroService that all application classes can use to get heroes"
    This reverts commit 20e3f0075e68c23dde4b13e449306eef86ee15b6.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
-import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,9 +8,9 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+  heroes = HEROES;
   selectedHero: Hero;
-  heroes: Hero[];
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
@@ -21,17 +20,12 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  constructor(private heroService: HeroService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.getHeroes();
-  }
-
-  getHeroes(): void{
-    this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes)
-  }
 
   
+
+  }
 
 }
