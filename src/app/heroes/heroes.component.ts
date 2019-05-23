20e3f0075e68c23dde4b13e449306eef86ee15b6
zoraiard <zoraiarodrigues@gmail.com>
2019-05-22 21:54:06 -0300

    reate a HeroService that all application classes can use to get heroes
    Services are a great way to share information among classes that don't know each other.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,9 +9,9 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+
   selectedHero: Hero;
-  
+  heroes: Hero[];
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
@@ -20,12 +21,17 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes)
+  }
 
   
-
-  }
 
 }
